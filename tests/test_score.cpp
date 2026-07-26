e61ea33215f4b24458532340bdc0e4da87f92ea8
ann4ann <anni4anny@gmail.com>
2026-07-26 16:21:51 +0300

    fix: restore score test expected value back to 10

--- tests/test_score.cpp
+++ tests/test_score.cpp
@@ -7,7 +7,7 @@ TEST_CASE("award_kill_score adds points_per_kill", "[score]")
     REQUIRE(config::score::points_per_kill == 10);
 
     int score = award_kill_score(0);
-    REQUIRE(score == 999);
+    REQUIRE(score == 10);
 
     score = award_kill_score(50);
     REQUIRE(score == 60);
